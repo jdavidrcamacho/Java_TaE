51863656b4315b1e29f9004ac9de5fb2666eaf4e
jdavidrcamacho <jdavidrcamacho@gmail.com>
2023-09-19 10:05:22 +0100

    first trials

---
+++ others/AreaCircle.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class AreaCircle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the radius of the circle: ");
+        double radius = scanner.nextDouble();
+        
+        double area = calculateCircleArea(radius);
+        System.out.println("The area of the circle is: " + area);
+        
+        scanner.close(); // Close the scanner to release resources
+    }
+
+    // Function to calculate the area of a circle
+    public static double calculateCircleArea(double radius) {
+        double pi = Math.PI; // Using the Math.PI constant for pi
+        double area = pi * (radius * radius);
+        return area;
+    }
+}
