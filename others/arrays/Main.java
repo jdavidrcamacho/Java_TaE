6d01b30a392e06c0649f9740b7cdaf075e610c9a
joao.camacho <joao.camacho@visionspace.com>
2023-11-21 16:53:57 +0000

    add arrays and lists

--- others/arrays/Main.java
+++ others/arrays/Main.java
@@ -1,5 +1,3 @@
-package dev.lpa;
-
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
--- others/linkedlists/Main.java
+++ others/linkedlists/Main.java
@@ -1,5 +1,3 @@
-package dev.lpa;
-
 import java.util.LinkedList;
 import java.util.ListIterator;
 
